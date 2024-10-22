fd318477842b3d43764deaaa1d38d206e380c3c0
Onta Nicolae <ontanicolae15@gmail.com>
2024-10-22 17:20:04 +0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,9 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Alege modul\n1 - FIFO\n2 - LIFO\n: ");
         int mod = scanner.nextInt();
-        System.out.print("Cuantumul de timp: ");
+        System.out.print("\nCuantumul de timp: ");
         int cuantum = scanner.nextInt();
-        System.out.print("Numarul de procese: ");
+        System.out.print("\nNumarul de procese: ");
         int n = scanner.nextInt();
 
         Proces[] procese = new Proces[n];
@@ -18,9 +18,18 @@ public class Main {
             procese[i].citire();
         }
 
+        System.out.print("\nProcesul cel mai lung: ");
+        Proces.longest(procese).afisare();
+
+        System.out.print("\nProcesul cel mai scurt: ");
+        Proces.shortest(procese).afisare();
+
+        System.out.print("\nToate procesele: ");
         for (Proces proces : procese) {
             proces.afisare();
         }
+
+        System.out.println("\n\nExecutarea " + (mod == 1 ? "LIFO" : "FIFO") + ":\n");
 
         if (mod == 1) {
             FIFO fifo = new FIFO(cuantum);
@@ -31,5 +40,10 @@ public class Main {
             LIFO lifo = new LIFO(cuantum);
             lifo.executare(procese);
         }
+
+        System.out.println("\nFunctii inventate, creative si in care vad sens:");
+        Proces.Inventeaza();
+        Proces.FiiCreativ();
+        Proces.VadSens();
     }
 }
--- src/Proces.java
+++ src/Proces.java
@@ -7,9 +7,15 @@ public class Proces {
     public Proces() {
     }
 
+    public Proces(String nume, int timpExecutie, int timpRamas) {
+        this.nume = nume;
+        this.timpExecutie = timpExecutie;
+        this.timpRamas = timpRamas;
+    }
+
     public void citire() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Introduceți numele procesului: ");
+        System.out.print("\nIntroduceți numele procesului: ");
         this.nume = scanner.nextLine();
         System.out.print("Introduceți timpul de execuție: ");
         this.timpExecutie = scanner.nextInt();
@@ -27,10 +33,42 @@ public class Proces {
             System.out.println("Executare proces " + nume + " timp " + timpExecutat + " unități.");
 
             if (timpRamas > 0) {
-                System.out.println("Timp rămas pentru proces " + nume + ": " + timpRamas);
+                System.out.println("Timp rămas pentru proces " + nume + ": " + timpRamas+"\n");
             } else {
-                System.out.println("Procesul " + nume + " a fost complet executat.");
+                System.out.println("Procesul " + nume + " a fost complet executat.\n");
             }
         }
+    }
+
+    public static Proces longest(Proces[] procese) {
+        Proces maxim = new Proces("temp",-999999999,-999999999);
+        for (Proces proces : procese) {
+            if (proces.timpExecutie > maxim.timpExecutie) {
+                maxim = proces;
+            }
+        }
+        return maxim;
+    }
+
+    public static Proces shortest(Proces[] procese) {
+        Proces minim = new Proces("temp",999999999,999999999);
+        for (Proces proces : procese) {
+            if (proces.timpExecutie < minim.timpExecutie) {
+                minim = proces;
+            }
+        }
+        return minim;
+    }
+
+    public static void Inventeaza() {
+        System.out.println("Am inventat");
+    }
+
+    public static void FiiCreativ() {
+        System.out.println("Am fost creativ");
+    }
+
+    public static void VadSens() {
+        System.out.println("Sens");
     }
 }
